f3bb25615bd6315764d1240d21bcf0807eb52c90
shino <shino@freedomcat.com>
2011-05-12 02:26:59 +0900

    delete double class(SendGMail).

--- hashtag2mail.rb
+++ hashtag2mail.rb
@@ -149,28 +149,6 @@ EOT
 	end
 end
 
-class SendGMail
-	def initialize(from, to, subject, body, user, pass, host = "smtp.gmail.com", port = 587)
-	  body = <<EOT
-From: #{from}
-To: #{to.to_a.join(",\n ")}
-Subject: #{NKF.nkf("-WMm0", subject)}
-Date: #{Time::now.strftime("%a, %d %b %Y %X %z")}
-Mime-Version: 1.0
-Content-Type: text/plain; charset=ISO-2022-JP
-Content-Transfer-Encoding: 7bit
-
-#{NKF.nkf("-Wjm0", body)}
-EOT
-
-  	Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-  	Net::SMTP.start(host, port, "localhost.localdomain", user, pass, "plain") do |smtp|
-		 smtp.send_mail body, from, to
- 	 end
-  end
-end
-
-
 class SendMail
 	def initialize(from, to, subject, body, host = "localhost", port = 25)
 	  body = <<EOT
